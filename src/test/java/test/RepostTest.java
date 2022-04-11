7f980f6d425362df4d114d02285391471ea44792
Komissarov Gleb <gl.k@aol.com>
2022-04-11 15:02:40 +0300

    Check background pic

--- src/test/java/test/RepostTest.java
+++ src/test/java/test/RepostTest.java
@@ -1,6 +1,5 @@
 package test;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.LoginPage;
@@ -18,6 +17,7 @@ public class RepostTest extends BaseTest {
     private static final String TEST_MESSAGE = "TEST TEST TEST!!!!";
     private static final String USER_ID = "600587939102";
     private static final String SAND_COLOR_RGB = "rgb(247, 196, 67)";
+    private static final String BACKGROUND_PIC = "url(\"//i.mycdn.me/i?r=A3FVj1t1rzgSLvsbLLN8xPEY2qvDEHE8faQ1rOSwuIFsyktH-I6dLXj4o-p3NCjoWJXv5WvSZFbEOnEQA05h_-Oge7THrgkDywWF44vljTmGcdkeZb6KfztSR4TYvmZPCv4rc0xrbcMbu58CWCBdQIRs\")";
 
     @Test
     void checkBackgroundColorForPost() {
@@ -26,7 +26,11 @@ public class RepostTest extends BaseTest {
         PostPage postPage = mainPage.receivePostPage();
         postPage.selectBeachDesign();
         postPage.sendPost(TEST_MESSAGE);
+
         ProfilePage profilePage = new ProfilePage(seleniumUtilities, USER_ID);
-        assertThat(profilePage.getStyleAttribute()).contains(SAND_COLOR_RGB);
+        String style = profilePage.getStyleAttribute();
+
+        assertThat(style).contains(SAND_COLOR_RGB);
+        assertThat(style).contains(BACKGROUND_PIC);
     }
 }
